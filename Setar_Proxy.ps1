88d899ba57925d86852497c1bd021818ec1ccfb3
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-10-24 16:22:40 -0400

    Segundo commit do arquivo

--- Setar_Proxy.ps1
+++ Setar_Proxy.ps1
@@ -1,7 +1,7 @@
 # Coloque o ip e porta do proxy que vc deseja que seja setado.
 $proxy = "10.10.10.10:8080"
 
-#pega o caminho de Internet Settings e permite alterar valores
+#pega o caminho de Internet Settings e permite alterar valores.
 $chave="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
 
 # faz o processo de alteração do proxy

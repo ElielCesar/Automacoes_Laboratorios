ac448c66aee1d9f3b522aea1f1bf15e88e94224b
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-11-03 10:56:24 -0400

    Segundo commit do arquivo

--- nomeacao_ifro_cheker.py
+++ nomeacao_ifro_cheker.py
@@ -1,4 +1,4 @@
-# Funcão - Realizar um crawler no site da ifro onde as nomeacões são feitas
+# Funcão - Realizar um crawler no site do ifro onde as nomeacões são feitas
 # e trazer o nome dos candidatos convocados.
 
 from bs4 import BeautifulSoup as bs

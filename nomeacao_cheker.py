9e06503ec1ff53de38456391dffcbeba47f37da1
Eliel Cesar da Silva Lima <eliel.tecinfo@gmail.com>
2019-11-03 10:54:24 -0400

    Primeiro commit do arquivo

---
+++ nomeacao_cheker.py
@@ -0,0 +1,21 @@
+# Funcão - Realizar um crawler no site da ifro onde as nomeacões são feitas
+# e trazer o nome dos candidatos convocados.
+
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+
+# requisitando a pagina
+html = urlopen(
+    'https://selecao.ifro.edu.br/tecnico-administrativo/537-concurso-2018-servidores-tecnico-administrativos-em-educacao')
+
+
+# usando o bs4
+concurso_page = bs(html, 'html.parser')
+div_convocacoes = concurso_page.find_all('div', {'class': 'phocadownloadfilelist'})
+nomeacoes = div_convocacoes[1].find_all('div', {'class': 'phocadownloadfilelist32'})
+
+# iterando sobre os resultados
+i = 0
+while i < len(nomeacoes):
+    print(nomeacoes[i].text)
+    i = i + 1
